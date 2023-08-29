97b798b41afb7f683624571203d40f5818a610be
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-29 06:55:31 +0200

    add: I'm up dating the app from the html ,adding a button to allow to dowload the files once treated

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
-// Supported file types
+javascript
+const fileInput = document.getElementById('file-input');
+const convertBtn = document.getElementById('convert-btn');
+const formatSelect = document.getElementById('format-select');
+
+convertBtn.addEventListener('click', () => {
+  const selectedFormat = formatSelect.value;
+  const file = fileInput.files[0];
+
+  const newFile = convertFile(file, selectedFormat);
+
+  // Rest of logic...
+});
+
+function convertFile(file, newType) {
+  const fileType = getFileType(file.name);
+
+  if (!fileTypes[fileType]) {
+    return {
+      error: 'Unsupported file type'
+    };
+  }
+
+  const newFile = new Blob([file], { type: fileTypes[newType] });
+
+  return newFile;
+}
+
+function getFileType(fileName) {
+  return fileName.split('.').pop();
+}
+
 const fileTypes = {
-    'jpg': 'image/jpeg',
-    'png': 'image/png', 
-    'pdf': 'application/pdf'
-  };
-  
-  // File converter function 
-  function convertFile(file, newType) {
-  
-    // Get file type from file name
-    const fileType = getFileType(file.name);
-  
-    // Check if original type is supported
-    if(!fileTypes[fileType]) {
-      return {
-        error: 'Unsupported file type' 
-      };
-    }
-  
-    // Convert to new type
-    const newFile = new Blob([file], {type: fileTypes[newType]});
-  
-    return newFile;
-  
-  }
-  
-  // Get file type from file name
-  function getFileType(fileName) {
-    return fileName.split('.').pop();
-  }
-  
-  
-  // Usage
-  
-  const fileInput = document.getElementById('file-input');
-  const convertBtn = document.getElementById('convert-btn');
-  
-  convertBtn.addEventListener('click', () => {
-  
-    const file = fileInput.files[0];
-    const newType = 'pdf'; // or any supported type
-  
-    const newFile = convertFile(file, newType);
-  
-    // Download new file
-    downloadFile(newFile, 'converted.' + newType); 
-  
-  });
+  'jpg': 'image/jpeg',
+  'png': 'image/png',
+  'pdf': 'application/pdf'
+};
+
+const downloadFile = (file, fileName) => {
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+};
+
+convertBtn.addEventListener('click', () => {
+  const file = fileInput.files[0];
+  const newType = 'pdf'; // or any supported type
+
+  const newFile = convertFile(file, newType);
+
+  // Download new file
+  downloadFile(newFile, 'converted.' + newType);
+});
